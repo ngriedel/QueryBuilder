ce5f685f75a13b30326d7ee3061bb4840d1a20c1
HassanWKSV2 <hassanmx@gmail.com>
2025-04-10 13:11:59 +0400

    u

---
+++ projects/query-builder/src/lib/models/query-builder.interfaces.ts
@@ -0,0 +1,159 @@
+export interface RuleSet {
+  condition?: string;
+  rules?: Array<RuleSet>;
+  collapsed?: boolean;
+  isChild?: boolean;
+
+  field?: string;
+  value?: any;
+  operator?: string;
+  entity?: string;
+}
+
+export interface Option {
+  name: string;
+  value: any;
+}
+
+export interface FieldMap {
+  [key: string]: Field;
+}
+
+export interface Field {
+  name: string;
+  value?: string;
+  type: string;
+  nullable?: boolean;
+  options?: Option[];
+  operators?: string[];
+  defaultValue?: any;
+  defaultOperator?: any;
+  entity?: string;
+  validator?: (rule: RuleSet, parent: RuleSet) => any | null;
+}
+
+export interface LocalRuleMeta {
+  ruleset: boolean;
+  invalid: boolean;
+}
+
+export interface EntityMap {
+  [key: string]: Entity;
+}
+
+export interface Entity {
+  name: string;
+  value?: string;
+  defaultField?: any;
+}
+
+export interface QueryBuilderClassNames {
+  arrowIconButton?: string;
+  arrowIcon?: string;
+  removeIcon?: string;
+  addIcon?: string;
+  button?: string;
+  buttonGroup?: string;
+  removeButton?: string;
+  removeButtonSize?: string;
+  switchRow?: string;
+  switchGroup?: string;
+  switchLabel?: string;
+  switchRadio?: string;
+  switchControl?: string;
+  rightAlign?: string;
+  transition?: string;
+  collapsed?: string;
+  treeContainer?: string;
+  tree?: string;
+  row?: string;
+  connector?: string;
+  rule?: string;
+  ruleSet?: string;
+  invalidRuleSet?: string;
+  emptyWarning?: string;
+  fieldControl?: string;
+  fieldControlSize?: string;
+  entityControl?: string;
+  entityControlSize?: string;
+  operatorControl?: string;
+  operatorControlSize?: string;
+  inputControl?: string;
+  inputControlSize?: string;
+}
+
+export interface QueryBuilderConfig {
+  fields: FieldMap;
+  entities?: EntityMap;
+  allowEmptyRulesets?: boolean;
+  getOperators?: (fieldName: string, field: Field) => string[];
+  getInputType?: (field: string, operator: string) => string;
+  getOptions?: (field: string) => Option[];
+  addRuleSet?: (parent: RuleSet) => void;
+  addRule?: (parent: RuleSet) => void;
+  removeRuleSet?: (ruleset: RuleSet, parent: RuleSet) => void;
+  removeRule?: (rule: RuleSet, parent: RuleSet) => void;
+  coerceValueForOperator?: (operator: string, value: any, rule: RuleSet) => any;
+  calculateFieldChangeValue?: (currentField: Field, nextField: Field, currentValue: any) => any;
+}
+
+export interface SwitchGroupContext {
+  onChange: (conditionValue: string) => void;
+  getDisabledState: () => boolean;
+  $implicit: RuleSet;
+}
+
+export interface EmptyWarningContext {
+  getDisabledState: () => boolean;
+  message: string;
+  $implicit: RuleSet;
+}
+
+export interface ArrowIconContext {
+  getDisabledState: () => boolean;
+  $implicit: RuleSet;
+}
+
+export interface EntityContext {
+  onChange: (entityValue: string, rule: RuleSet) => void;
+  getDisabledState: () => boolean;
+  entities: Entity[];
+  $implicit: RuleSet;
+}
+
+export interface FieldContext {
+  onChange: (fieldValue: string, rule: RuleSet) => void;
+  getFields: (entityName: string) => void;
+  getDisabledState: () => boolean;
+  fields: Field[];
+  $implicit: RuleSet;
+}
+
+export interface OperatorContext {
+  onChange: () => void;
+  getDisabledState: () => boolean;
+  operators: string[];
+  $implicit: RuleSet;
+}
+
+export interface InputContext {
+  onChange: () => void;
+  getDisabledState: () => boolean;
+  options: Option[];
+  field: Field;
+  $implicit: RuleSet;
+}
+
+export interface ButtonGroupContext {
+  addRule: () => void;
+  addRuleSet: () => void;
+  removeRuleSet: () => void;
+  getDisabledState: () => boolean;
+  $implicit: RuleSet;
+}
+
+export interface RemoveButtonContext {
+  removeRule: (rule: RuleSet) => void;
+  getDisabledState: () => boolean;
+  $implicit: RuleSet;
+}
---
+++ projects/query-builder/src/public-api.ts
@@ -0,0 +1,20 @@
+/*
+ * Public API Surface of query-builder
+ */
+
+export * from './lib/directives/query-arrow-icon.directive';
+export * from './lib/directives/query-button-group.directive';
+export * from './lib/directives/query-empty-warning.directive';
+export * from './lib/directives/query-entity.directive';
+export * from './lib/directives/query-field.directive';
+export * from './lib/directives/query-input.directive';
+export * from './lib/directives/query-operator.directive';
+export * from './lib/directives/query-remove-button.directive';
+export * from './lib/directives/query-switch-group.directive';
+export * from './lib/models/query-builder.interfaces';
+export * from './lib/query-builder.component';
+export * from './lib/services/query-builder.service';
+
+
+
+
